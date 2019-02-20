a81964b4fe535a9b262cc1d8d391f91153855073
vupnguyen55 <vupnguyen55@gmail.com>
2019-02-20 15:37:57 -0500

    heroku fix

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const app = express();
 
-const PORT = 8080;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
